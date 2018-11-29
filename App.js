acbb17132c690d1ef9558258ff28dadf3984a46a
Трефилов Андрей <trefilovandew@yandex.ru>
2018-11-29 16:26:00 +0500

    cool

--- App.js
+++ App.js
@@ -1,161 +1,115 @@
-import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  PanResponder,
-  LayoutAnimation,
-} from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet, Animated, ScrollView, Dimensions } from 'react-native';
+import { Constants } from 'expo';
+
+const { height, width } = Dimensions.get('window')
+
+const data = [1,2,3,4,5,6,7,8,9,10]
 
 export default class App extends React.Component {
   state = {
-    list: [
-      { top: 0, text: 1 },
-      { top: 0, text: 2 },
-      { top: 0, text: 3 },
-      { top: 0, text: 4 },
-      { top: 0, text: 5 },
-      { top: 0, text: 6 },
-      { top: 0, text: 7 },
-      { top: 0, text: 8 },
-      { top: 0, text: 9 },
-      { top: 0, text: 10 },
-      { top: 0, text: 11 },
-      { top: 0, text: 12 },
-      { top: 0, text: 'Конец' },
-    ],
-    curIndex: 0,
+    page: 0
   }
 
-  way = ''
+  page = 0
 
-  componentWillUpdate() {
-    LayoutAnimation.easeInEaseOut()
-  }
-
-  renderBlock = (e, eIndex) => {
-    const index = this.state.curIndex
-    const p = (PanResponder.create({
-      onStartShouldSetPanResponder: () => true,
-      onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
-      onMoveShouldSetPanResponder: (evt, gestureState) => { },
-      onMoveShouldSetPanResponderCapture: (evt, gestureState) => { },
-      onPanResponderGrant: () => { },
-      onPanResponderMove: (evt, gestureState) => {
-        const { list } = this.state
-        const result = list.slice()
-
-        if (this.way === '') {
-          if (gestureState.dy > 0) {
-            this.way = 'down'
-          } else {
-            this.way = 'up'
-          }
-        }
-  
-        if (eIndex !== index) {
-          return
-        }
-  
-        if (this.way === 'up' && e.top <= 0 && eIndex !== list.length - 1) {
-          result[index].top = result[index].top + gestureState.dy
-        }
-
-        if (
-          this.way === 'down'
-          && result[index - 1]
-          && result[index - 1].top >= -225
-          && result[index - 1].top <= 0
-        ) {
-          result[index - 1].top = result[index - 1].top + gestureState.dy
-        }
-  
-        this.setState({ list: result })
-      },
-      onPanResponderTerminationRequest: () => true,
-      onPanResponderRelease: (evt, gestureState) => {
-        const { list } = this.state
-        const result = list.slice()
-        if (this.way === 'up') {
-          if (result[index].top < -200 * 0.3) {
-            result[index].top = -220
-            this.setState({ curIndex: index + 1, list: result })
-          } else {
-            result[index].top = 0
-    
-            this.setState({ list: result })
-          }
-        }
-
-        if (this.way === 'down' && result[index - 1]) {
-          if (result[index - 1].top > -200 * 0.7) {
-            result[index - 1].top = 0
-            this.setState({ curIndex: index - 1, list: result })
-          } else {
-            result[index - 1].top = -220
-    
-            this.setState({ list: result })
-          }
-        }
-
-        this.way = ''
-      },
-      onPanResponderTerminate: (evt, gestureState) => { },
-      onShouldBlockNativeResponder: () => false,
-    }))
-    return (
-      <View
-        {...p.panHandlers}
-        style={[styles.block, {
-          top: (eIndex - index) * 10 + e.top,
-          zIndex: (eIndex - index) * -1,
-          width: 200,
-          transform: [{ scaleX: (1 - 0.1 * (eIndex - index)) }],
-          opacity: (eIndex - index) < 3 ? 1 : 0
-        }]}
-        key={eIndex}
-      >
-        <Text>{e.text}</Text>
-      </View>
-    )
-  }
-
+  scroll = new Animated.Value(0)
   render() {
-    const { list } = this.state
-
     return (
       <View style={styles.container}>
-        <View style={styles.list}>
-          {
-            list.map((e, index) => this.renderBlock(e, index))
-          }
+        <View style={styles.box}>
+          {this.card()}
         </View>
+        <ScrollView
+          decelerationRate={'fast'}
+          onScroll={Animated.event([{ nativeEvent: { contentOffset: { y: this.scroll } } }])}
+          scrollEventThrottle={16}
+          snapToInterval={height}
+          showsVerticalScrollIndicator={false}
+        >
+          {data.map(() => (
+            <View key={`${Math.random()}`} style={{ width, height }} />
+          ))}
+        </ScrollView>
       </View>
     );
+  }
+
+  card = () => {
+    return data.map((item, index) => {
+      const inputRange = [height * (index - 2), height * (index - 1), height * index, height * (index + 1)]
+      const startOut = index > 2 ? 50 : 25 * index
+      const secondOut = index > 1 ? 25 : 25 * index
+
+      const translateY = this.scroll.interpolate({
+        inputRange,
+        outputRange: [startOut, secondOut, 0, -(height / 1.5)],
+        extrapolate: 'clamp'
+      })
+
+      let scaleX = 1
+
+      if (index !== 0) {
+        scaleX = this.scroll.interpolate({
+          inputRange,
+          outputRange: [0.8, 0.9, 1, 1],
+          extrapolate: 'clamp'
+        })
+      }
+
+      const opacity = this.scroll.interpolate({
+        inputRange: [height * (index - 3), ...inputRange],
+        outputRange: [0, 1, 1, 1, 1],
+        extrapolate: 'clamp'
+      })
+
+      return (
+        <Animated.View
+          key={`${Math.random()}`}
+          style={[
+            styles.card,
+            {
+              opacity,
+              transform: [
+                { translateY },
+                { scaleX }
+              ],
+              zIndex: -index
+            }
+          ]}
+        />
+      )
+    })
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
   },
-  list: {
-    overflow: 'hidden',
-    paddingTop: 20,
-    height: 240,
-    width: 202,
-  },
-  block: {
-    width: 200,
-    height: 200,
-    borderWidth: 1,
-    borderColor: '#333',
-    backgroundColor: '#FFF',
+  card: {
+    height: height / 2,
+    width: height / 2,
+    backgroundColor: 'white',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 10
+    },
+    shadowRadius: 3,
+    shadowOpacity: 0.2,
     position: 'absolute',
+    borderRadius: 6
+  },
+  box: {
+    width,
+    height,
     alignItems: 'center',
     justifyContent: 'center',
-  },
+    backgroundColor: 'red',
+    position: 'absolute'
+  }
 });
